089c79ea873d85f5f48bca157411f1ee4b482ff6
zhouGTM <965021018@qq.com>
2021-05-25 22:29:25 +0800

    Update TestGit.java

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -2,7 +2,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("qqqq");
         System.out.println("qqqq");
-        System.out.println("qqqq");
-        System.out.println("qqqq");
+       
     }
 }
