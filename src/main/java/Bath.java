22329f5ffafd7ffb2fe24d7f1ae1e46691d43ac2
zhouGTM <965021018@qq.com>
2021-05-27 21:33:31 +0800

    测试提交代码

---
+++ src/main/java/Bath.java
@@ -0,0 +1,8 @@
+/**
+ * 测试Git提交代码
+ */
+public class Bath {
+    public static void main(String[] args) {
+        System.out.println("狗子，到哪儿了？");
+    }
+}
--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -7,6 +7,9 @@ public class TestGit {
         System.out.println("qqqq");
         System.out.println("qqqq");
         System.out.println("qqqq");
+        System.out.println("啊实打实");
+        System.out.println("啊实打实大苏打撒旦");
+        System.out.println("阿三大苏打实打实");
 
 
     }
